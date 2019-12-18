01c0b43d7661429958f05e7c50efbf04bbb1dec0
Miguel Fernandez <miguelrf95@gmail.com>
2019-12-18 16:18:36 +0000

    Add doubleBuffer pattern tests

--- DoubleBuffer2D/Program.cs
+++ DoubleBuffer2D/Program.cs
@@ -4,9 +4,35 @@ namespace DoubleBuffer2D
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            DoubleBuffer2D<char> buffer = 
+                new DoubleBuffer2D<char>(5, 5);
+
+            buffer[1, 0] = 'X';
+            buffer[2, 0] = 'X';
+            buffer[3, 0] = 'X';
+            buffer[4, 0] = 'X';
+            buffer[1, 1] = 'X';
+            buffer[1, 2] = 'X';
+            buffer[2, 2] = 'X';
+            buffer[3, 2] = 'X';
+            buffer[3, 3] = 'X';
+            buffer[0, 4] = 'X';
+            buffer[1, 4] = 'X';
+            buffer[2, 4] = 'X';
+            buffer[3, 4] = 'X';
+
+            buffer.Swap();
+
+            for (int y = 0; y < buffer.YDim; y++)
+            {
+                for (int x = 0; x < buffer.XDim; x++)
+                {
+                    Console.Write(buffer[x,y]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
